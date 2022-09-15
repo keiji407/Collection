beb2c1a6718b5da555dea2b1a0453f0d20d0d6f1
KeijiTaguchi <110805798+keiji407@users.noreply.github.com>
2022-09-15 10:40:20 +0900

    課題提出

--- src/practice/Chapter5.java
+++ src/practice/Chapter5.java
@@ -1,10 +1,31 @@
 package practice;
 
-public class Chapter5 {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class Chapter5 {
 	public static void main(String[] args) {
 		
-		
+		Task t1 = new Task(LocalDate.of(2021,10,21),"牛乳を買う。");
+	    Task t2 = new Task(LocalDate.of(2021,9,15),"○○社面談。");
+	    Task t3 = new Task(LocalDate.of(2021,12,4),"手帳を買う。");
+	    Task t4 = new Task(LocalDate.of(2021,8,10),"散髪に行く。");
+	    Task t5 = new Task(LocalDate.of(2021,11,9),"スクールの課題を解く。");
+	    
+	    List<Task> task = new ArrayList<Task>();
+	    task.add(t1);
+	    task.add(t2);
+	    task.add(t3);
+	    task.add(t4);
+	    task.add(t5);
+	   
+	    Collections.sort(task);
+	    
+	    for(int i=0;i<task.size();i++){
+			System.out.println(task.get(i));
+	    }
 
 	}
 
--- src/practice/Task.java
+++ src/practice/Task.java
@@ -1,5 +1,44 @@
 package practice;
 
-public class Task {
+import java.time.LocalDate;
+
+public class Task implements Comparable<Task>{
+	//implements Comparable<Task>
+	
+	private LocalDate date;
+	private String content;
+	
+	public Task(LocalDate date ,String content) {
+		this.date = date;
+		this.content = content;
+	}
+	
+	public LocalDate getDate() {
+		return date;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+	@Override
+	public int compareTo(Task t) {
+		return this.date.compareTo(t.date);
+	}
+	
+	@Override
+	public String toString() {
+		return this.date + " " + this.content;
+	}
+	
 
 }
